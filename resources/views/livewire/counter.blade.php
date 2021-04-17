0f002a794f38cdfaf41e15ad55902d7a5ba80793
Lucas Andrade <lucascorporation@live.com>
2021-04-16 22:30:26 -0300

    first commit

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div style="text-align: center">
+        <button wire:click="increment">+</button>
+        <h1>{{ $count }}</h1>
+    </div>
+@endsection
+
